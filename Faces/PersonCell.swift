120d454cfeb2d106c6d0913a7a2766c82222e0d6
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2021-10-11 17:14:14 +0300

    added collection view controller

---
+++ Faces/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  Faces
+//
+//  Created by Abdulrahman on 10/11/21.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView : UIImageView!
+    @IBOutlet weak var labelView: UILabel!
+    
+    
+}
