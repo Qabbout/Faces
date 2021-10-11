3a2d2a367a944d86508d366a0e91f13772ebb281
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2021-10-11 20:13:36 +0300

    added image picker

--- Faces/ViewController.swift
+++ Faces/ViewController.swift
@@ -1,19 +1,53 @@
-//
-//  ViewController.swift
-//  Faces
-//
-//  Created by Abdulrahman on 10/11/21.
-//
+    //
+    //  ViewController.swift
+    //  Faces
+    //
+    //  Created by Abdulrahman on 10/11/21.
+    //
 
 import UIKit
 
-class ViewController: UICollectionViewController {
-
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate{
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addPerson))
     }
-
-
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell")
+        }
+        return cell
+    }
+    
+    @objc func addPerson(){
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+        
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[.editedImage] as? UIImage else {return}
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 1){
+            try? jpegData.write(to: imagePath)
+        }
+        dismiss(animated: true)
+    }
+    
+    func getDocumentsDirectory() -> URL{
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
 }
 
